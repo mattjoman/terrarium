fb831f07135ff76e4f0f5cf3c31ed667651faf81
Your Name <matt.j.penney@gmail.com>
2022-05-12 21:34:06 +0100

    animals are now classes

--- core/predator.h
+++ core/predator.h
@@ -2,9 +2,10 @@
 
 #include "animal.h"
 
-struct Predator : Animal {
-	Predator(int id, std::vector<float> pos, int min_death_age, int max_death_age);
-	void inc_hunger();
-	void print();
-	~Predator();
+class Predator : public Animal {
+	public:
+		Predator(int id, std::vector<float> pos, int min_death_age, int max_death_age);
+		void inc_hunger();
+		void print();
+		~Predator();
 };
--- core/prey.h
+++ core/prey.h
@@ -3,9 +3,10 @@
 #include "animal.h"
 #include "../share/includes.h"
 
-struct Prey : Animal {
-	Prey(int id, std::vector<float> pos, int min_death_age, int max_death_age);
-	void inc_hunger();
-	void print();
-	~Prey();
+class Prey : public Animal {
+	public:
+		Prey(int id, std::vector<float> pos, int min_death_age, int max_death_age);
+		void inc_hunger();
+		void print();
+		~Prey();
 };
